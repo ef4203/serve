fa22f6415f31e181f97acfe7561444b888612196
Elias F <32647755+ef4203@users.noreply.github.com>
2019-08-25 00:37:19 +0200

    Added vscode tasks and added comments to string_ex

--- src/string_ex.c
+++ src/string_ex.c
@@ -1,27 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+/* Functions to make working with strings more sane. */
+
 #include <stdlib.h>
 #include <string.h>
 
-/* Defines the string keyword. */
+/* Define the string keyword. */
 typedef char *string;
 
 /* Append SRC onto DEST, using reallocation. */
-int strapp(char **__dest, const char *__src)
+string strapp(char **__dest, const char *__src)
 {
     string __new = (char *)malloc(
         (strlen(*__dest) + strlen(__src)) * sizeof(char) + 1);
+
+    if (!__new)
+        return NULL; /* Memory allocation failed. */
 
     strcpy(__new, *__dest);
     strcat(__new, __src);
     free(*__dest);
     *__dest = __new;
 
-    return 1;
+    return *__dest;
 }
 
 /* Create a new dynamic string. */
 string strnew()
 {
     string __new = (char *)malloc(sizeof(char));
+
+    if (!__new)
+        return NULL; /* Memory allocation failed. */
 
     memset(__new, 0, 1);
 
--- src/string_ex.h
+++ src/string_ex.h
@@ -1,13 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+/* Functions to make working with strings more sane. */
+
 #ifndef STRING_EX_H
 #define STRING_EX_H
 
-/* Defines the string keyword. */
+/* Define the string keyword. */
 typedef char *string;
 
 /* Append SRC onto DEST, using reallocation. */
-extern int strapp(char **__dest, const char *__src);
+extern string strapp(char **__dest, const char *__src);
 
 /* Create a new dynamic string. */
-extern char *strnew();
+extern string strnew();
 
-#endif
+#endif /* STRING_EX_H */
